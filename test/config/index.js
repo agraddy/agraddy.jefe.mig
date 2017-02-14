e2e9767518ff6b105e55a6352ccd1b71a81a2216
Anthony Graddy <anthony@locationq.com>
2017-02-14 23:32:28 +0000

    Added LONGTEXT

---
+++ test/config/index.js
@@ -0,0 +1,10 @@
+var output = {};
+
+output.db = {};
+
+output.db.host = 'localhost';
+output.db.user = 'root';
+output.db.password = '';
+output.db.database = 'change_me';
+
+module.exports = output;
